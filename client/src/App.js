379f1e542e7379c5632725cf9f20f41737d2d0b7
Megan <mennis0145@gmail.com>
2019-09-16 19:21:25 +0100

    Save movie functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,18 @@ const App = () => {
       <SavedList list={savedList} />
       <div>
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        {/* <Route path="/movies/:id" component={Movie} addToSavedList={addToSavedList} /> */}
+        <Route
+        path="/movies/:id"  
+          render={props => (
+            <Movie
+              addToSavedList={addToSavedList}
+              match={props.match}
+              history={props.history}
+              location={props.location}
+            />
+          )}
+        />
       </div>
     </div>
   );
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -10,6 +10,8 @@ const Movie = props => {
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
+    const addToSavedList = props.addToSavedList;
+
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => {
@@ -18,13 +20,13 @@ const Movie = props => {
       .catch(error => {
         console.error(error);
       });
-  }, []);
+  }, [props.addToSavedList, props.match.params.id]);
 
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -40,7 +42,9 @@ const Movie = props => {
         metascore={metascore}
         stars={stars}
       />
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={evt=> saveMovie(movie)}>
+        Save
+      </div>
     </div>
   );
 };
