bc657b877350729c8c6548d3de6a97f2b3e3ce72
Megan <mennis0145@gmail.com>
2019-09-16 14:06:05 +0100

    MovieCard component created & imported to relevant files to remove repetition

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import MovieCard from "./MovieCard";
 
 const Movie = props => {
   const [movie, setMovie] = useState(null);
-  console.log(props);
-  console.log(props.match.params.id);
+
   useEffect(() => {
     const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
@@ -31,25 +31,15 @@ const Movie = props => {
   }
 
   const { title, director, metascore, stars } = movie;
-  console.log(title);
+
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard
+        title={title}
+        director={director}
+        metascore={metascore}
+        stars={stars}
+      />
       <div className="save-button">Save</div>
     </div>
   );
